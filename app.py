0115b1e5cbfe187493990cb4b75fca2434b5611e
PramishBytes <pramishadhikari30@gmail.com>
2026-07-17 23:44:30 +0545

    Update: API_TOKEN added

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, jsonify, request
 import sqlite3
 import hashlib
+import os
+import dotenv
 
+API_TOKEN = os.getenv("APU_TOKEN")
 
 app = Flask(__name__)
 
+def require_token(f):
+    @wraps(f)
+    def decorated_function(*args,**kwargs):
+        token = request.headers.get("Authorization")
+        if token != f"Bearer{API_TOKEN}":
+            return jsonify({"message":"Unauthorized"}),401
+        return f(*args,**kwargs)
+
+    return decorated_function
+
 def get_db_connection():
-    import os
+    
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     db_path = os.path.join(BASE_DIR, "products.db")
     
